f1d0f272952b083c4d15aaf8016cb9b10eabcdce
TristoneGit <arifuzzamansifat12@gmail.com>
2026-04-13 00:11:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "#components/Navbar.jsx"
+
+const App = () => {
+  return (
+    <main>
+      {/* Changed navBar to Navbar */}
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
